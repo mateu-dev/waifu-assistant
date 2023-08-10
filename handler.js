73a60653e6c6565d6354f87c812715dd866f4dfb
Mateusz Ciesielski <73233340+MCM4STER@users.noreply.github.com>
2023-08-10 17:09:54 +0200

    json

--- handler.js
+++ handler.js
@@ -1,24 +1,32 @@
 const chrono = require("chrono-node");
 const fs = require("fs");
 
-const LIST_OF_COMMANDS = fs.readFileSync()
+const LIST_OF_COMMANDS = JSON.parse(fs.readFileSync("./commands.json"));
+const CONFIG = JSON.parse(fs.readFileSync("./config.json"));
 
 module.exports.handleRequest = async function (req) {
     req = decodeURIComponent(req.command);
     const time = chrono.parseDate(req);
-    let response = await fetch("http://localhost:8000/message?q=" + encodeURIComponent(req));
     console.log(req);
-    response = decodeUnicode(await response.text());
+    if (CONFIG.gpt.gpt_commands) {
+        // Handle picking commands with gpt
+        return;
+    }
+    let response = await fetch("http://localhost:8000/message?q=" + encodeURIComponent(req));
+    response = await response.text();
     console.log(response);
     return response;
 }
 
+
+// Fix this later
 function decodeUnicode(text) {
-    return text.replace(/\\u[\dA-Fa-f]{4}|[^\\u]+/g, match => {
-        if (match.startsWith("\\u")) {
-            return String.fromCharCode(parseInt(match.substr(2), 16));
-        } else {
-            return eval('"' + match + '"');
-        }
-    });
+    decodeURIComponent(text)
+    // return text.replace(/\\u[\dA-Fa-f]{4}|[^\\u]+/g, match => {
+    //     if (match.startsWith("\\u")) {
+    //         return String.fromCharCode(parseInt(match.substr(2), 16));
+    //     } else {
+    //         return eval('"' + match + '"');
+    //     }
+    // });
 }
