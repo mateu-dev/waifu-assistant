b032f7047acdeefa04ac69f3229d77685ccbfbb7
mateu <73233340+MCM4STER@users.noreply.github.com>
2023-08-11 01:30:23 +0200

    t

--- handler.js
+++ handler.js
@@ -4,19 +4,57 @@ const fs = require("fs");
 const LIST_OF_COMMANDS = JSON.parse(fs.readFileSync("./commands.json"));
 const CONFIG = JSON.parse(fs.readFileSync("./config.json"));
 
-module.exports.handleRequest = async function (req) {
-    req = decodeURIComponent(req.command);
-    const time = chrono.parseDate(req);
-    console.log(req);
+for (const command of LIST_OF_COMMANDS)
+    command.regex = generateRegExpAndExtractors(command.commands[0]);
+
+module.exports.handleRequest = async function (input) {
+    input = decodeURIComponent(input.command);
+    const time = chrono.parseDate(input);
+    console.log(input);
     if (CONFIG.gpt.gpt_commands) {
         // Handle picking commands with gpt
         return;
     }
-    let response = await fetch("http://localhost:8000/message?q=" + encodeURIComponent(req));
-    response = await response.text();
-    console.log(response);
+
+    const current_command = LIST_OF_COMMANDS.find((command) => { return command.regex.regex.test(input) })
+    current_command.variables = current_command.regex.extractors(input);
+    console.log(current_command)
+
+    let response = "gpt not enabled";
+    if (CONFIG.gpt.enabled) {
+        response = await fetch("http://localhost:8000/message?q=" + encodeURIComponent(input));
+        response = await response.text();
+    }
+
+    // console.log(response);
     return response;
 }
+
+function generateRegExpAndExtractors(template) {
+    const variables = [];
+    const regexString = template.replace(/<([^>]+)>/g, (_, variable) => {
+        variables.push(variable);
+        return "(.*?)";
+    });
+
+    const regex = new RegExp(`^${regexString}$`);
+
+    return {
+        regex,
+        extractors: input => {
+            const match = input.match(regex);
+            if (match) {
+                return variables.reduce((result, variable, index) => {
+                    result[variable] = match[index + 1];
+                    return result;
+                }, {});
+            } else {
+                return null;
+            }
+        }
+    };
+}
+
 
 
 // Fix this later
